016141f0010ccf9a5964d8a1b61e5cbda3356005
Damien Boizard <boizarddamien751@gmail.com>
2022-11-14 13:41:07 +0100

    Final de la finalité

--- android/src/com/kgame/game/AndroidLauncher.java
+++ android/src/com/kgame/game/AndroidLauncher.java
@@ -1,18 +1,40 @@
 package com.kgame.game;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.kgame.game.KGame;
 
 public class AndroidLauncher extends AndroidApplication {
+	private static Context context;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useAccelerometer = false; // Désactivation de l'accéléromètre
-		config.useCompass = false; // Désactivation du compas
-		initialize(new KGame(), config);
+		context = this;
+
+		boolean launchGame = getIntent().getBooleanExtra("launchGame", false);
+
+		if (launchGame)
+		{
+			AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+			config.useAccelerometer = false; // Désactivation de l'accéléromètre
+			config.useCompass = false; // Désactivation du compas
+			initialize(new KGame(), config);
+		}
+		else
+		{
+			Intent launcherActivityIntent = new Intent(this, MainMenuActivity.class);
+			ContextCompat.startActivity(this, launcherActivityIntent, null);
+			finish();
+		}
+	}
+
+	public static Context getAppContext() {
+		return context;
 	}
 }
